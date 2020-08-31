16b2eff4eff907abab13c580359efbc72ab6004c
Iliana Portugal <ilianaportugal@Ilianas-MacBook-Air.local>
2020-08-30 23:10:11 -0700

    add files

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,95 @@
+-- Retrieve columns from Employees table
+SELECT emp_no, first_name, last_name
+FROM employees
+
+-- Retrieve columns from Titles 
+SELECT title, from_date, to_date
+FROM titles
+
+-- New table
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+retirement_titles
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+ORDER BY _____, _____ DESC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- Count Titles
+SELECT COUNT (emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT DESC;
+
+-- Retrieve columns from Employees table 
+SELECT emp_no, first_name, last_name, birth_date
+FROM employees;
+
+-- Retrieve columns from Department Employee table
+SELECT from_date, to_date
+FROM dept_emp;
+
+-- Retrieve columns from Titles table
+SELECT title
+FROM titles;
+
+-- Create new table
+SELECT DISTINCT ON (emp_no),
+e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+-- INTO 
+FROM employees as e
+JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+
+-- Create Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no, de.from_date DESC;
